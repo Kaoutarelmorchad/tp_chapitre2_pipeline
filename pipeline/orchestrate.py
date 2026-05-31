e14f1e83974998ceba5d7041210b176537ef3683
kaoutar El morchad <kaoutarelmorchad1@gmail.com>
2026-05-31 16:22:53 +0100

    Ajout de l'orchestration Dagster

---
+++ pipeline/orchestrate.py
@@ -0,0 +1,25 @@
+﻿from dagster import job, op
+import os
+
+@op
+def ingest():
+    os.system("python pipeline/ingest.py")
+
+@op
+def validate():
+    os.system("python pipeline/validate.py")
+
+@op
+def transform():
+    os.system("cd dbt_pipeline && dbt run --profiles-dir .")
+
+@op
+def test_data():
+    os.system("cd dbt_pipeline && dbt test --profiles-dir .")
+
+@job
+def ventes_pipeline():
+    ingest()
+    validate()
+    transform()
+    test_data()
