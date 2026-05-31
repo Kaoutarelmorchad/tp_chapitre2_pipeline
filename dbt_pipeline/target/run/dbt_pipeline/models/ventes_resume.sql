abeb9f4e3b0189839b8e18cbb9ee0c6dbe0adab8
kaoutar El morchad <kaoutarelmorchad1@gmail.com>
2026-05-31 16:00:26 +0100

    Ajout des modèles dbt

---
+++ dbt_pipeline/target/run/dbt_pipeline/models/ventes_resume.sql
@@ -0,0 +1,10 @@
+
+  
+  create view "ventes"."main"."ventes_resume__dbt_tmp" as (
+    ﻿select
+    categorie,
+    sum(quantite) as total_quantite,
+    sum(chiffre_affaires) as total_ca
+from "ventes"."main"."ventes_clean"
+group by categorie
+  );
